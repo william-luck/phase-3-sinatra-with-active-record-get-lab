2613ceb8d0daca460cb0b3b80abc03430ecc3708
William Luck <luck.william.h@gmail.com>
2022-09-15 16:59:10 -0300

    commit tree e937e7504bf0c9a0b3e9ac7080d5ea49452c18e2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do 
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do 
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods_by_price = BakedGood.all.order(price: :desc)
+    baked_goods_by_price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.all.order(price: :desc).first
+    most_expensive.to_json
+  end
 
 end
